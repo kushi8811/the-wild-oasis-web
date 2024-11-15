3d8ef2b1aa274bd47043d10d57cbde31f7d644a7
kushi8811 <gjorgjokushi1998@icloud.com>
2024-11-15 18:30:31 +0100

    Normalize line endings 2

--- app/account/layout.js
+++ app/account/layout.js
@@ -2,11 +2,11 @@ import SideNavigation from "../_components/SideNavigation";
 
 export default function Layout({ children }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[16rem_1fr] h-full gap-12">
-      <div className="sm:col-span-2 sm:flex justify-center">
+    <div className="grid grid-cols-1 sm:grid-cols-[1fr] lg:grid-cols-[16rem_1fr] h-full gap-12">
+      <div className="lg:sticky lg:top-[-2rem] lg:h-screen lg:w-[16rem] z-10 sm:w-full lg:ml-[-2rem]">
         <SideNavigation />
       </div>
-      <div className="py-4 px-4 sm:px-6 md:px-8">
+      <div className="py-4 px-4 sm:px-6 md:px-8 lg:mr-[14rem] lg:pt-6">
         <div>{children}</div>
       </div>
     </div>
